9a2d65aedf3f9ce0dd59424f38ce45102a66ea0c
Saurav Khare <saurav.erahk@gmail.com>
2019-12-27 14:28:09 +0530

    Moved to backticks

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 App = {
   web3Provider: null,
   contracts: {},
-  account: '0x0',
+  account: `0x0`,
   hasVoted: false,
 
   init: function() {
@@ -10,21 +10,21 @@ App = {
 
   initWeb3: function() {
     // TODO: refactor conditional
-    if (typeof web3 !== 'undefined') {
+    if (typeof web3 !== `undefined`) {
       // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       ethereum.enable();
       web3 = new Web3(web3.currentProvider);
     } else {
       // Specify default instance if no web3 instance provided
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      App.web3Provider = new Web3.providers.HttpProvider(`http://localhost:7545`);
       web3 = new Web3(App.web3Provider);
     }
     return App.initContract();
   },
 
   initContract: function() {
-    $.getJSON("Election.json", function(election) {
+    $.getJSON(`Election.json`, function(election) {
       // Instantiate a new truffle contract from the artifact
       App.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
@@ -44,9 +44,9 @@ App = {
       // https://github.com/MetaMask/metamask-extension/issues/2393
       instance.votedEvent({}, {
         fromBlock: 0,
-        toBlock: 'latest'
+        toBlock: `latest`
       }).watch(function(error, event) {
-        console.log("event triggered", event)
+        console.log(`event triggered`, event)
         // Reload when a new vote is recorded
         App.render();
       });
@@ -55,8 +55,8 @@ App = {
 
   render: function() {
     var electionInstance;
-    var loader = $("#loader");
-    var content = $("#content");
+    var loader = $(`#loader`);
+    var content = $(`#content`);
 
     loader.show();
     content.hide();
@@ -65,7 +65,7 @@ App = {
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
+        $(`#accountAddress`).html(`Your Account: ` + account);
       }
     });
 
@@ -74,10 +74,10 @@ App = {
       electionInstance = instance;
       return electionInstance.candidatesCount();
     }).then(function(candidatesCount) {
-      var candidatesResults = $("#candidatesResults");
+      var candidatesResults = $(`#candidatesResults`);
       candidatesResults.empty();
 
-      var candidatesSelect = $('#candidatesSelect');
+      var candidatesSelect = $(`#candidatesSelect`);
       candidatesSelect.empty();
 
       for (var i = 1; i <= candidatesCount; i++) {
@@ -87,11 +87,11 @@ App = {
           var voteCount = candidate[2];
 
           // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          var candidateTemplate = `<tr><th>` + id + `</th><td>` + name + `</td><td>` + voteCount + `</td></tr>`
           candidatesResults.append(candidateTemplate);
 
           // Render candidate ballot option
-          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          var candidateOption = `<option value=` + id +  `>` + name + `</ option>`
           candidatesSelect.append(candidateOption);
         });
       }
@@ -99,7 +99,7 @@ App = {
     }).then(function(hasVoted) {
       // Do not allow a user to vote
       if(hasVoted) {
-        $('form').hide();
+        $(`form`).hide();
       }
       loader.hide();
       content.show();
@@ -109,13 +109,13 @@ App = {
   },
 
   castVote: function() {
-    var candidateId = $('#candidatesSelect').val();
+    var candidateId = $(`#candidatesSelect`).val();
     App.contracts.Election.deployed().then(function(instance) {
       return instance.vote(candidateId, { from: App.account });
     }).then(function(result) {
       // Wait for votes to update
-      $("#content").hide();
-      $("#loader").show();
+      $(`#content`).hide();
+      $(`#loader`).show();
     }).catch(function(err) {
       console.error(err);
     });
